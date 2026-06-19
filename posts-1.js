a8eb274eb286dfe275f02586b3d4cbdc1d570732
wow <seleman66eeddwegger3@gmail.com>
2026-06-19 11:10:53 +0800

    笔记: 修正 Ubuntu 24.04 LTS 括号说明

--- posts-1.js
+++ posts-1.js
@@ -35,7 +35,7 @@ https://hostinger.com/WOWINSIGHT
 
 ## 操作系统选择
 - **必须选 Linux**（Ubuntu，不要选带 GUI 的发行版）
-- 推荐选 **Ubuntu 24.04 LTS**（24.04 不是 22.04）
+- 推荐选 **Ubuntu 24.04 LTS**
 - 推荐选带 **Docker 预装**的模板，一键省去安装 Docker 的步骤
 
 ## 节点选择
